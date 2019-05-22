0d8cde241e7d86e770559f5ae29e6c4e5ed76912
Anton33 <Anton33@139.174.106.195>
2019-05-22 10:16:57 +0200

    kki

--- GitHub/src/Package/HelloWorld.java
+++ GitHub/src/Package/HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World 1");
 		System.out.println("Hello World 3");
+		System.out.println("Hello World 13");
+
 	}
 
 }
