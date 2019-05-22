3f51cd87f27bf25e02b22838493e948b38691841
Anton33 <Anton33@139.174.106.195>
2019-05-22 09:58:26 +0200

    Initial

---
+++ GitHub/src/Package/HelloWorld.java
@@ -0,0 +1,11 @@
+package Package;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World 1");
+		System.out.println("Hello World 2");
+	}
+
+}
